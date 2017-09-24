193b980725ec40b2d8af68232c14e7ec7b279309
Blitzwolfer <anthonyvu.98169@gmail.com>
2017-09-24 05:45:09 -0400

    Update baselibrary.lua

--- cardlibrary/baselibrary.lua
+++ cardlibrary/baselibrary.lua
@@ -16692,9 +16692,9 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Cost"] = {["Red"] = 3,["Neutral"] = 1,},
 		["Effect"] = {
 			Name = "Magic Trick!",
-			Description = "At the end of your turns, materialise a Hodif.",
+			Description = "At the end of your turns, summon a Kitten out of the magician's hat.",
 			["Type"] = "OnEnd",
-			["Power"] = {{"Summon","Hodif"}},
+			["Power"] = {{"Summon","Kitten"}},
 			Target = "Ally",
 		},
 		["Bio"] = "I shall pull great things using my magic hat.",
