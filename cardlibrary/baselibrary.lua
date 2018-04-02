667eecfc839c6bc856aac089e3f8dcf173cff718
WalrusOverlord <WalrusOverlord@users.noreply.github.com>
2018-04-02 02:53:31 -0400

    dayren

--- cardlibrary/baselibrary.lua
+++ cardlibrary/baselibrary.lua
@@ -541,6 +541,12 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Dayren"] = {
 		["Id"] = 264620119,
 		["Name"] = "Dayren",
+		["AltCards"] = {
+			["Dayren-E"] = {
+				["Id"] = 1567571638,
+				["Bio"] = "A nice little jig on his drums was all he needed to amass an army of rabbits.",
+			},
+		},
 		["Power"] = 250,
 		["Health"] = 250,
 		["Rarity"] = "Uncommon",
@@ -989,6 +995,11 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Id"] = 271751037,
 		["Name"] = "Chef",
 		["AltCards"] = {
+			["Chef-E"] = {
+				["Id"] = 1567570823,
+				["Name"] = "Egg Chef",
+				["Bio"] = "If you aren’t in the mood for lunch, breakfast is still available. Delicious Robloxian eggs, cooked to perfection, sunny side up.",
+			},
 			["Baker"] = {
 				["Id"] = 1247061539,
 				["Name"] = "Baker",
@@ -5828,6 +5839,12 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Blockerwiz"] = {
 		["Id"] = 294871375,
 		["Name"] = "Blockerwiz",
+		["AltCards"] = {
+			["Blockerwiz-E"] = {
+				["Id"] =  1567564871,
+				["Bio"] = "Remember kids: eating raw eggs could give you salmonella poisoning! The symptoms of salmonella include chills, dehydration, diarrhea, fatigue, fever, headaches, loss of appetite, and pain areas in the abdomen or muscles.",
+			},
+		},
 		["Health"] = 900,
 		["Power"] = 450,
 		["Rarity"] = "Common",
@@ -8595,6 +8612,21 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan's Stone"] = {
 		["Id"] = 328831705,
 		["Name"] = "Titan's Stone",
+		["AltCards"] = {
+			["Titan's Stone-E"] = {
+				["Name"] = "Bunny Blob",
+				["Id"] = 1567565788,
+				["Bio"] = "Just a harmless bunny blob. Nothing more. Totally can’t summon giant titan blobs or anything. Totally not.",
+				["Effect"] = {
+					Name = "Stud",
+					Description = "Can't generate icons. Set the power of this fighter to 0 at the end of your turns.",
+					["Type"] = "OnEnd",
+					["Power"] = "Weaken",
+					Target = "Self",
+					Increase = 9999,
+				},
+			},
+		},
 		["Health"] = 500,
 		["Power"] = 0,
 		["Rarity"] = "Common",
@@ -8617,6 +8649,21 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Recruiter of the Titans"] = {
 		["Id"] = 328831691,
 		["Name"] = "Recruiter of the Titans",
+		["AltCards"] = {
+			["Recruiter-E"] = {
+				["Name"] = "Recruiter of the Bunny Blobs",
+				["Id"] = 1567582533,
+				["Bio"] = "Evewybody, pwease, get in hewe! I would be ewew gwateful if you did! uwu",
+				["Effect"] = {
+					Name = "Stud",
+					Description = [[At the end of of your turns, put a Bunny Blob into your hand. This card can't generate icons.]],
+					["Type"] = "OnEnd",
+					["Power"] = "Add",
+					Target = "Ally",
+					Increase = "Titan's Stone-E",
+				},
+			},
+		},
 		["Health"] = 600,
 		["Power"] = 0,
 		["Rarity"] = "Uncommon",
@@ -8626,7 +8673,7 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Cost"] = {["Neutral"] = 3,},
 		["Effect"] = {
 			Name = "Stud",
-			Description = [[At the end of of your turns, put a "Titan's Stone" to your hand. This card can't generate icons.]],
+			Description = [[At the end of of your turns, put a Titan's Stone into your hand. This card can't generate icons.]],
 			["Type"] = "OnEnd",
 			["Power"] = "Add",
 			Target = "Ally",
@@ -8638,6 +8685,21 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan of the Infernal Abyss"] = {
 		["Id"] = 328831698,
 		["Name"] = "Titan of the Infernal Abyss",
+		["AltCards"] = {
+			["Titan of the Infernal Abyss-E"] = {
+				["Name"] = "Bunny Blob of the Infernal Abyss",
+				["Id"] = 1567569380,
+				["Bio"] = "Guarding a pit of fire is pretty lame. Guarding an empty pit is even lamer.",
+				["Effect"] = {
+					Name = "Stud",
+					Description = "You must control at least 1 Bunny Blob to summon this card. When this card is summoned, deal 500 damage to all Non-Blobs.",
+					["Type"] = "OnSummon",
+					["Power"] = "Damage",
+					Target = "NotArchetype",
+					Increase = 500,
+				},
+			},
+		},
 		["Health"] = 2000,
 		["Power"] = 0,
 		["Rarity"] = "Rare",
@@ -8660,6 +8722,21 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan of the Deep Below"] = {
 		["Id"] = 328831715,
 		["Name"] = "Titan of the Deep Below",
+		["AltCards"] = {
+			["Titan of the Deep Below-E"] = {
+				["Name"] = "Bunny Blob of the Deep Below",
+				["Id"] = 1567566958,
+				["Bio"] = "April showers bring May flowers. April floods ruin harvests of spuds.",
+				["Effect"] = {
+					Name = "Stud",
+					Description = "You must have at least 1 Bunny Blob in play to summon this card. When this card is summoned, lock all Non-Blobs for 3 turns.",
+					["Type"] = "OnSummon",
+					["Power"] = "Lock",
+					Target = "NotArchetype",
+					Increase = 3,
+				},
+			},
+		},
 		["Health"] = 2000,
 		["Power"] = 0,
 		["Rarity"] = "Rare",
@@ -8682,6 +8759,21 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan of the Dying Forest"] = {
 		["Id"] = 328831713,
 		["Name"] = "Titan of the Dying Forest",
+		["AltCards"] = {
+			["Titan of the Dying Forest-E"] = {
+				["Name"] = "Bunny Blob of the Dying Forest",
+				["Id"] = 1567567466,
+				["Bio"] = "Easter is a celebration of life and rebirth, yet so much material is wasted wrapping candy eggs. Hypocritical, really.",
+				["Effect"] = {
+					Name = "Stud",
+					Description = "You must control at least 1 Bunny Blob to summon this card. When this card is summoned, all Blobs gain 1000 Health.",
+					["Type"] = "OnSummon",
+					["Power"] = "Heal",
+					Target = "Archetype",
+					Increase = 1000,
+				},
+			},
+		},
 		["Health"] = 2000,
 		["Power"] = 0,
 		["Rarity"] = "Rare",
@@ -8692,7 +8784,7 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Cost"] = {["Green"] = 5,},
 		["Effect"] = {
 			Name = "Stud",
-			Description = "You must control at least 1 titan to summon this card. When this card is summoned, all Titan(s) gain 1000 Health.",
+			Description = "You must control at least 1 titan to summon this card. When this card is summoned, all Titans gain 1000 Health.",
 			["Type"] = "OnSummon",
 			["Power"] = "Heal",
 			Target = "Archetype",
@@ -8704,6 +8796,20 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan of the Fried One"] = {
 		["Id"] = 328831709,
 		["Name"] = "Titan of the Fried One",
+		["AltCards"] = {
+			["Titan of the Fried One-E"] = {
+				["Name"] = "Bunny Blob of the Fried One",
+				["Id"] = 1567568071,
+				["Bio"] = "Rabbits are higher on the food chain now…",
+				["Effect"] = {
+					Name = "Stud",
+					Description = "You must control at least 1 Bunny Blob to summon this card. Increase the health and power of all Blobs by 500.",
+					["Type"] = "OnSummon",
+					["Power"] = {{"Heal",500},{"Strengthen",500}},
+					Target = "Archetype",
+				},
+			},
+		},
 		["Health"] = 2000,
 		["Power"] = 0,
 		["Rarity"] = "Rare",
@@ -8725,6 +8831,20 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 	["Titan of the Holy Chamber"] = {
 		["Id"] = 328831700,
 		["Name"] = "Titan of the Holy Chamber",
+		["AltCards"] = {
+			["Titan of the Holy Chamber-E"] = {
+				["Name"] = "Bunny Blob of the Holy Chamber",
+				["Id"] = 1567568604,
+				["Bio"] = "The arrival of titan signalled the beginning of the nega-rapture: The spirits of all blobbunnies would coalesce and form the Blobgotten One.",
+				["Effect"] = {
+					Name = "Stud",
+					Description = [[You must control at least 1 Bunny Blob to summon this card. Set the power of all non-Blobs to 0. Put "The Blobgotten One" to your hand.]],
+					["Type"] = "OnSummon",
+					["Power"] = {{"SetPower",0},{"Add","The Blobgotten One"}},
+					Target = "NotArchetype",
+				},
+			},
+		},
 		["Health"] = 2000,
 		["Power"] = 0,
 		["Rarity"] = "Rare",
@@ -8735,9 +8855,9 @@ local base = { -- CARD_ID, NAME, POWER, HEALTH, RARITY,BIO
 		["Cost"] = {["Neutral"] = 5,},
 		["Effect"] = {
 			Name = "Stud",
-			Description = [[You must control at least 1 titan to summon this card. Set the power of all non-Titans to 200. Put "The Forgotten One" to your hand.]],
+			Description = [[You must control at least 1 titan to summon this card. Set the power of all non-Titans to 0. Put "The Forgotten One" to your hand.]],
 			["Type"] = "OnSummon",
-			["Power"] = {{"SetPower",200},{"Add","The Forgotten One"}},
+			["Power"] = {{"SetPower",0},{"Add","The Forgotten One"}},
 			Target = "NotArchetype",
 		},
 		["Bio"] = "Probably just gonna be standard anti-aggro used in every second deck, but the thought is nice.",
