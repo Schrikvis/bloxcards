d589135722a4a2965bf3236a7cbe488813eb4d8b
WalrusOverlord <WalrusOverlord@users.noreply.github.com>
2018-06-18 09:33:51 -0400

    Update packs.lua

--- packs.lua
+++ packs.lua
@@ -184,6 +184,7 @@ local packs = {
 		CardPackId = 842435184, --721400845,
 		Description = "Red is the colour of power, ambition, and control. Dominion packs contain cards that cost red!",
 		Cards = {
+			"Magical Girl Histor",
 			"Melon Desu",
 			"ZeDerpyDonal",
 			"Sands of Purgatory",
@@ -1056,6 +1057,7 @@ local packs = {
 		Description = "Exotic new cards from the black market! White cards and joke cards! It's all yours, as long as you have 10 wins!",
 		WinRequirement = 10;
 		Cards = {
+			"Magical Girl Histor",
 			"SnailMan39",
 			"Vinrole",
 			"Unc, Uncorrupted",
@@ -1137,7 +1139,6 @@ local packs = {
 			"Roundawsome",
 			"Wizardguy34",
 			"Zabiehunter",
-			"Mlgloga",
 			-- PAST THIS POINT IS FOR WHITE CARDS AND JOKE CARDS ONLY. I'M LOOKING AT YOU, BLITZ.
 			"saud20",
 			"SpeedySeat",
